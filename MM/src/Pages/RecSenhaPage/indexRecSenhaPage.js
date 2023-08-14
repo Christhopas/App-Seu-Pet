bcf184c757b6ac3374df69bc5b794d86ffe6adb6
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2023-08-14 20:03:45 -0300

    Create indexRecSenhaPage.js

---
+++ MM/src/Pages/RecSenhaPage/indexRecSenhaPage.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function RecSenhaPage(){
+    return(
+        <View style = {styles.container}>
+          <Text> oi </Text>
+        </View>
+    )
+}
