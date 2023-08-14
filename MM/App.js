28b1f3e16864f4b80364bb1a55900575b78ea67a
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2023-08-14 19:55:53 -0300

    Upload2

---
+++ MM/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import ConquistasPage from './src/Pages/ConquistasPage/indexConquistas';
+import InicialPage2 from './src/Pages/InicialPage2/indexInicialPage2';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Página inicial 2" component={InicialPage2} options={{headerShown: false}} />
+        <Stack.Screen name="Página de conquistas" component={ConquistasPage} options={{headerShown: false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
