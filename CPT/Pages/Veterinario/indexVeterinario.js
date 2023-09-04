9465228bc4d7ebd3bb0a7a0a5cc0f534dc23c843
Christopher Thompson <101826879+Christhopas@users.noreply.github.com>
2023-09-03 21:50:23 -0300

    Update indexVeterinario.js

--- CPT/Pages/Veterinario/indexVeterinario.js
+++ CPT/Pages/Veterinario/indexVeterinario.js
@@ -6,70 +6,57 @@ import { Link } from '@react-navigation/native';
 
 export default function VeterinarioPage(){
 
-    var [ isPress, setIsPress ] = useState(false);
-    var [ isPress2, setIsPress2 ] = useState(false);
+  var [ isPress, setIsPress ] = useState(false);
+  var [ isPress2, setIsPress2 ] = useState(false);
 
-    var Voltar = {
-        activeOpacity: 1,
-        underlayColor: '#270505',                              
-        style: isPress ? styles.btnPress : styles.btnNormal, 
-        onHideUnderlay: () => setIsPress(false),
-        onShowUnderlay: () => setIsPress(true),
-      };
+  var Voltar = {
+    activeOpacity: 1,
+    underlayColor: "#D16262",                              
+    style: isPress ? styles.btnPress : styles.btnNormal, 
+    onHideUnderlay: () => setIsPress(false),
+    onShowUnderlay: () => setIsPress(true),
+    onPress: () => console.log('AU AU ou outro barulho qualquer'),  //  "onPress" é obrigatório, pelo menos foi o q eu entendi
+  };
 
-      var Avancar = {
-        activeOpacity: 1,
-        underlayColor: '#D16262',                              
-        style: isPress2 ? styles.btnPress : styles.btnNormal, 
-        onHideUnderlay: () => setIsPress2(false),
-        onShowUnderlay: () => setIsPress2(true),
-      };
-
-     
-
-    return (
-        <View style = {styles.container}>
-            <View style = {styles.viewimg}>
-                {/* Ajustar o tamnho pra fical igual a de td mundo*/}
-                <Image style = {styles.img} source ={require('../../assets/Icons/vet.png')}/> 
-            </View>  
+  return (
+    <View style = {styles.container}>
+      <View style = {styles.viewimg}>
+        <Image style = {styles.img} source ={require('../../assets/Icons/vet.png')}/> 
+    </View>  
             
-            <Text style = {styles.txt}>Veterinario</Text>
+    <Text style = {styles.txt}>Veterinario</Text>
             
-            <View style = {styles. viewCima}>
-              <View style = {styles. imgEsq}>
-                  {/* Colocar o link para a tela do pet Shop*/}
-                  <Link to= {{screen: ""}}> <Image style = {styles.img} source = {require('../../assets/Icons/seupet.png')}/> </Link>
-                  <Text style = {styles.txt}>Seu Pet</Text>
-              </View>
-              <View style = {styles. imgDir}>
-                {/* Colocar o link para a tela do veterinário*/}
-                <Link to= {{screen: ""}}> <Image style = {styles.img} source = {require('../../assets/Icons/rastreamento.png')}/> </Link>
-                <Text style = {styles.txt}>Rastreamento</Text>
-              </View>  
-              </View>
-              <View style = {styles. viewBaixo}>
-                <View style = {styles. imgEsq}>
-                <Link to= {{screen: ""}}> <Image style = {styles.img} source = {require('../../assets/Icons/calendariopet.png')}/></Link>
-                <Text style = {styles.txt}>calendário Pet</Text>
-              </View>  
-              <View style = {styles. imgDir}>
-                <Link to= {{screen: ""}}> <Image style = {styles.img} source = {require('../../assets/Icons/Alimentacao.png')}/> </Link>
-                <Text style = {styles.txt}>Alimentação</Text>
-              </View>
-              </View>
-            
-            <View style = {styles.ViewBtn}>
-                <TouchableHighlight {...Voltar} style = {styles.VoltarBtn}>
-                    <Link to= {{screen: "Página inicial 2"}} style = {styles.txtVBtn}>voltar</Link>
-                </TouchableHighlight>
-            </View>
-            <View style = {styles.ViewABtn}>
-                <TouchableHighlight {...Avancar}  style = {styles.AvancarBtn}>
-                    <Link to= {{screen: "Página inicial 2"}} style = {styles.txtABtn}>{"<"}</Link>
-                </TouchableHighlight>
-            </View>
+    <View style = {styles. viewCima}>
+      <View style = {styles. imgEsq}>
+        <Link to= {{screen: ""}}> <Image style = {styles.img} source = {require('../../assets/Icons/seupet.png')}/> </Link>
+        <Text style = {styles.txt}>Seu Pet</Text>
+      </View>
+
+      <View style = {styles. imgDir}>
+        <Link to= {{screen: ""}}> <Image style = {styles.img} source = {require('../../assets/Icons/rastreamento.png')}/> </Link>
+        <Text style = {styles.txt}>Rastreamento</Text>
+      </View>  
+    </View>
+
+      <View style = {styles. viewBaixo}>
+        <View style = {styles. imgEsq}>
+          <Link to= {{screen: ""}}> <Image style = {styles.img} source = {require('../../assets/Icons/calendariopet.png')}/></Link>
+          <Text style = {styles.txt}>calendário Pet</Text>
         </View>
-    )
+
+        <View style = {styles. imgDir}>
+          <Link to= {{screen: ""}}> <Image style = {styles.img} source = {require('../../assets/Icons/Alimentacao.png')}/> </Link>
+          <Text style = {styles.txt}>Alimentação</Text>
+        </View>
+      </View>
+            
+      <View style = {styles.viewVoltarBtn}>
+        <TouchableHighlight {...Voltar}  style = {styles.voltarBtn}>
+          <Link to= {{screen: "Página de Recuperar Senha"}} style = {styles.txtVoltarBtn}>{"<"}</Link>  
+        </TouchableHighlight>
+      </View>
+
+    </View>
+  )
 }
 
