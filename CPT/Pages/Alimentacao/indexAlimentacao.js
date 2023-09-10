2a6ddbbb7efd28d4452591f9551037139494150a
Christopher Thompson <101826879+Christhopas@users.noreply.github.com>
2023-09-10 20:13:49 -0300

    Update indexAlimentacao.js

--- CPT/Pages/Alimentacao/indexAlimentacao.js
+++ CPT/Pages/Alimentacao/indexAlimentacao.js
@@ -6,18 +6,30 @@ import { Link } from '@react-navigation/native';
 
 export default function AlimentacaoPage(){
 
+  const headers = ['Peso', 'Quantidade de ração'];
+  const rowSpanData = [
+    ['Peso'], ['Quantidade de Ração'],
+  ];
 
 
   return (
 
     <View style = {styles.container}>
       <View style = {styles.viewimg}>
-        <Image style = {styles.img} source ={require('../../assets/Icons/Alimentacao.png')}/> 
-    </View>  
-            
-    <Text style = {styles.txt}>Alimentação</Text>
-            
+        <Image style = {styles.img} source ={require('../../assets/Icons/Alimentacao.png')}/>         
+        <Text style = {styles.txt}>Alimentação</Text>
+      </View>
 
+
+      <View style={styles.tabela}>
+        <Image style={styles.tabela} source={require('../../assets/Icons/tabela.png')}/>
+      </View>
+      
+    <View>
+ 
+
+
+    </View>
     </View>
   )
 }
