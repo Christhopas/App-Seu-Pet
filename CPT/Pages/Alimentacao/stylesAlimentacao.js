b9184596aa5326b198359dad8f333109011b803c
Christopher Thompson <101826879+Christhopas@users.noreply.github.com>
2023-09-13 22:48:21 -0300

    Update stylesAlimentacao.js

--- CPT/Pages/Alimentacao/stylesAlimentacao.js
+++ CPT/Pages/Alimentacao/stylesAlimentacao.js
@@ -1,164 +1,121 @@
 import { Dimensions, StyleSheet } from "react-native";
 
-const MAX_WIDTH = Dimensions.get('window').width;
 const MAX_HEIGHT = Dimensions.get('window').height;
+const MAX_WIDTH = Dimensions.get('window').width;
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
+
     container: {
         flex: 1,
-        flexDirection: 'column',
-        backgroundColor: "#63316D",
-        justifyContent: 'space-around',
+        backgroundColor: '#63316D',
+        justifyContent: 'center',
         alignItems: 'center',
-    
     },
 
+    img: {
+        width: 125,
+        height: 125,
+        marginTop: 10,
+        borderRadius: 20,
+    },
 
+    icone: {
+        backgroundColor: '#D18162',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 25,
+        height: 145,
+        width: 145,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.5,
+        shadowRadius: 4,
+        elevation: 5,
+        marginTop: 10,
+        display: 'flex',
+    },
 
     tabela: {
         justifyContent: 'center',
         alignItems: 'center',
         borderRadius: 15,
         overflow: 'hidden',
-        height: MAX_HEIGHT * 0.40,
-        width: MAX_WIDTH * 1,
+        height: windowHeight * 0.40,
+        width: windowWidth * 1,
         margin: 10,
-      },
-
-
-    viewImg: {
-        justifyContent:'center',
-        alignItems: 'center',
     },
-
-    viewCima: {
-        flex: 1,
-        display: 'flex',
-        flexDirection: 'row',
-        marginBottom: 70,
-        marginTop: 70
-       
-    },
-
-    viewBaixo: {
-        flex: 1,
-        display: 'flex',
-        flexDirection: 'row',
-        margin: 10
-    },
-
-    img: {
-        height: MAX_HEIGHT * 0.12,
-        width: MAX_WIDTH * 0.24,
-        tintColor: "#63316D",
-        margin: 10,
-        backgroundColor: "#D18162",
-        borderRadius: 10,
-        
-    },
-
-    imgEsq: {
-        marginLeft: 20,
-        marginRight: 20,
-       
-    },
-
-    imgDir: {
-        marginLeft: 20,
-        marginRight: 20,
-        
-    },
-
 
     txt: {
-        color: '#fff',
-        fontSize: 20,
+        color: 'white',
+        fontSize: windowWidth * 0.07,
         fontWeight: 'bold',
-        justifyContent: 'center',
-        alignItems: 'center',
-        textAlign: 'center'
+        textAlign: 'center',
+        marginTop: 5,
+        marginBottom: 10,
     },
 
-    viewImgOsso: {
-        display: 'flex',
-        width:'100%',
-        alignItems: 'center', 
+
+    voltar: {
+        alignSelf: 'flex-start',
+        paddingHorizontal: windowHeight * 0.02,
+        paddingTop: windowHeight * 0.07,
+        borderRadius: 20,
+        overflow: 'hidden',
     },
 
-    osso: {
-        display: 'flex',
-        height: MAX_HEIGHT * 0.045,
-        width: MAX_WIDTH * 0.110,
-        tintColor: "#D18162",
-        margin: 10,
-        right: '40%',
-        top: '5%'
+    txtBtn: {
+        color: '#fff',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 20,
+        
     },
 
-    txtOsso:{
-        color: "#fff",
-        fontWeight: "bold",
-        fontSize: 16,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        bottom: '4%',
-        right: '30%',
-        marginLeft: 30
-    },
-
-    
-    viewAvançarBtn: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignSelf: 'flex-end',
-        top: '40%',
-        marginLeft: 20,
-    },
-
-    avançarBtn: {
-        backgroundColor: "#F8EC7D",
+    buttonVoltar: {
+        backgroundColor: "#6F1C1C",
+        width: windowWidth * 0.25,
+        height: windowHeight * 0.04,
         borderRadius: 10,
-        width: MAX_WIDTH * 0.25,
-        height: MAX_HEIGHT * 0.04,
+        justifyContent: 'center',
+        marginBottom: 10,
         shadowColor: "#000",
         shadowOffset: {width: 0, height: 4},
         shadowOpacity: 0.6,
-        alignItems: 'center',
-        justifyContent: 'center',
+        marginLeft: windowWidth * 0.1,
+        marginTop: windowWidth * 0.20
     },
 
-    txtAvançarBtn: {
-        color: "#D18162",
-        fontWeight: 'bold',
+    buttons: {
+        alignSelf: 'flex-start',
+    },
+
+    viewimg: {
+        marginTop: MAX_HEIGHT * 0.015,
+        backgroundColor: "#D18162",
+        borderRadius: 25,
+        marginBottom: 80,
+        width: MAX_WIDTH * 0.45,
+        height: MAX_HEIGHT * 0.2,
+    },
+    
+    img: {
+        height: 165,
+        width: 165,
+        margin: 10,
+        tintColor: "#63316D",
+    },
+    
+    txt: {
+        color: "#fff",
+        fontSize: 24,
+        fontWeight: "bold",
         textAlign: 'center',
-        fontSize: 20
+        fontSize: 30,
+        marginTop: 15
     },
-
-    viewSairBtn: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignSelf: 'flex-end',
-        marginRight: 20,
-        bottom: '106%'
-    },
-
-    sairBtn: {
-        backgroundColor: "#6F1C1C",
-        width: MAX_WIDTH * 0.25,
-        height: MAX_HEIGHT * 0.04,
-        borderRadius: 10,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-
-    txtSairBtn: {
-        color: '#fff',
-        textAlign: 'center',
-        fontWeight: 'bold',
-        fontSize: 20
-    }
-
 
 
 });
