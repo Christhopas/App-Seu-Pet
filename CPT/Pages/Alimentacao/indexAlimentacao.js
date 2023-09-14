f46ef18ed803dbeb8d40a73a3e8f7bfb6395d033
Christopher Thompson <101826879+Christhopas@users.noreply.github.com>
2023-09-13 22:47:58 -0300

    Update indexAlimentacao.js

--- CPT/Pages/Alimentacao/indexAlimentacao.js
+++ CPT/Pages/Alimentacao/indexAlimentacao.js
@@ -1,4 +1,4 @@
-import { View, Image, Text, TouchableHighlight, TouchableOpacity } from "react-native";
+import { View, Image, Text, TouchableHighlight, TouchableOpacity, SafeAreaView, Button } from "react-native";
 import styles from "./stylesAlimentacao";
 import { useState } from "react"
 import { Link } from '@react-navigation/native';
@@ -6,31 +6,44 @@ import { Link } from '@react-navigation/native';
 
 export default function AlimentacaoPage(){
 
-  const headers = ['Peso', 'Quantidade de ração'];
-  const rowSpanData = [
-    ['Peso'], ['Quantidade de Ração'],
-  ];
+  var [ isPress, setIsPress ] = useState(false);
+
+  var Voltar = {
+    activeOpacity: 1,
+    underlayColor: '#270505',                              
+    onHideUnderlay: () => setIsPress(false),
+    onShowUnderlay: () => setIsPress(true),
+    onPress: () => console.log('Botão de voltar acionado'),
+  };
+
+
 
 
   return (
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.viewimg}>
+          <Image style={styles.img} source={require('../../assets/Icons/Alimentacao.png')} />
+          <Text style={styles.txt}> Alimentação </Text>
+        </View>
 
-    <View style = {styles.container}>
-      <View style = {styles.viewimg}>
-        <Image style = {styles.img} source ={require('../../assets/Icons/Alimentacao.png')}/>         
-        <Text style = {styles.txt}>Alimentação</Text>
-      </View>
-
-
-      <View style={styles.tabela}>
-        <Image style={styles.tabela} source={require('../../assets/Icons/tabela.png')}/>
-      </View>
-      
-    <View>
- 
+        <View style={styles.tabela}>
+          <Image style={styles.tabela} source={require('../../assets/Icons/tabela.png')}/>
+        </View>
 
 
-    </View>
-    </View>
+        <View style={styles.buttons}>
+          <TouchableHighlight {...Voltar} style={styles.buttonVoltar}>
+            <Link to = {{screen: "InicialPage1"}} style ={styles.txtBtn}> Voltar </Link>
+          </TouchableHighlight>
+        </View>
+
+      </SafeAreaView>
+    
+    
+    
+    
+    
+    </>
   )
 }
-
